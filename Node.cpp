c13296e1fa900d330f8506dc4d651fdf130139bc
FlagerLee <46860743+FlagerLee@users.noreply.github.com>
2019-04-28 23:16:01 +0800

    Add files via upload

---
+++ Node.cpp
@@ -0,0 +1,146 @@
+#include <string>
+#include <map>
+#include <cmath>
+#include <iostream>
+#include <cstdio>
+#include "Node.h"
+
+using namespace std ;
+
+map < string , Node* > Var_map ;
+
+//Node类
+
+void Node::add_next ( Node* N ) { Node::next.push_back ( N ) ; }
+void Node::del ()
+{
+    int len = next.size () ;
+    for ( int i = 0 ; i < len ; i ++ ) delete next [ i ] ;
+    vector < Node* > ().swap ( next ) ;
+}
+
+//#end#
+
+//Calculator类
+
+template <typename _Tp_>
+double Calculator::get_value ( _Tp_ T )
+{
+    if ( T.name == "var" || T.name == "placeholder" || T.name == "constant" || T.name == "var_constant" ) return T.value ;
+    else std::cout << "Error: " << T.name << "does not have value\n" ;
+}
+
+template <typename _Tp_>
+void Calculator::eval ( double v , _Tp_ T )
+{
+    if ( T.name == "var" || T.name == "placeholder" || T.name == "var_constant" ) T.value = v ;
+    else std::cout << "Error: You cannot evaluate a " << T.name << "\n" ;
+}
+
+double Unary_Operator::cal ( string s , double v , bool& is_legal )
+{
+    if ( s == "SIN" ) return sin ( v ) ;
+    else if ( s == "EXP" ) return exp ( v ) ;
+    else if ( s == "TANH" ) return ( exp ( v ) - exp ( -v ) ) / ( exp ( v ) + exp ( -v ) ) ;
+    else if ( s == "SIGMOID" ) return 1.0 / ( 1.0 + exp ( -v ) ) ;
+    else if ( s == "LOG" )
+    {
+        if ( v > 0 ) return log ( v ) ;
+        else { is_legal = false ; cout << "Error: LOG operator's input must be positive\n" ; return 0.0 ; }
+    }
+    else if ( s == "PRINT" )
+    {
+        cout << "Print Operator: " << s << "=" << v << "\n" ;
+        return v ;
+    }
+    return 0.0 ;
+}
+
+double Binary_Operator::cal ( string s , double v1 , double v2 , bool& is_legal )
+{
+    switch ( s [ 0 ] )
+    {
+        case '+' : return v1 + v2 ;
+        case '-' : return v1 - v2 ;
+        case '*' : return v1 * v2 ;
+        case '/' :
+        {
+            if ( v2 == 0 )
+            {
+                is_legal = false ;
+                cout << "ERROR: Division by zero\n" ;
+                return 0.0 ;
+            }
+            else return v1 / v2 ;
+        }
+        case '<' :
+        {
+            if ( s.length() == 1 )
+            {
+                if ( v1 < v2 ) return 1.0 ;
+                else return 0.0 ;
+            }
+            else if ( s [ 1 ] == '=' )
+            {
+                if ( v1 <= v2 ) return 1.0 ;
+                else return 0.0 ;
+            }
+        }
+        case '>' :
+        {
+            if ( s.length() == 1 )
+            {
+                if ( v1 > v2 ) return 1.0 ;
+                else return 0.0 ;
+            }
+            else if ( s [ 1 ] == '-' )
+            {
+                if ( v1 >= v2 ) return 1.0 ;
+                else return 0.0 ;
+            }
+        }
+        case '=' :
+        {
+            if ( s.length() == 1 )
+            {
+                is_legal = false ;
+                cout << "Error: Cannot evaluate in an expression\n" ;
+                return 0.0 ;
+            }
+            else
+            {
+                if ( v1 == v2 ) return 1.0 ;
+                else return 0.0 ;
+            }
+        }
+        default :
+        {
+            is_legal = false ;
+            cout << "Error: No matching operators for '" << s << "'\n" ;
+            return 0.0 ;
+        }
+    }
+    return 0.0 ;
+}
+
+double Ternary_Operator::cal ( string s , double v1 , double v2 , double v3 , bool& is_legal )
+{
+    if ( s == "COND" )
+    {
+        return v1 ? v2 : v3 ;
+    }
+    else
+    {
+        is_legal = false ;
+        cout << " Error: No matching operators for '" << s << "'\n" ;
+        return 0.0 ;
+    }
+    return 0.0 ;
+}
+
+//#end#
+
+void build_tree ( string exp ) //根据输入的结点建树
+{
+    
+}
