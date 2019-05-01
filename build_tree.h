782f391ddc902c26f77c1bda79587928dadff2f8
FlagerLee <46860743+FlagerLee@users.noreply.github.com>
2019-05-01 10:32:44 +0800

    Update build_tree.h

--- build_tree.h
+++ build_tree.h
@@ -10,6 +10,8 @@ inline int priority ( std::string c ) ;
 
 Node* create_calculator ( std::string s , int& count_arg ) ;
 
+void delete_tree ( Node* N ) ;
+
 Var* build_tree( std::string s, std::map < std::string , Node* > Var_map ) ;
 
 Node* connect ( std::vector<std::string> s , std::map<std::string , Node*> Var_map , int head , int tail ) ;
