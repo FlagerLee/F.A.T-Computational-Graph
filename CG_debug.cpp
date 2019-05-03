fbd80faa8b728e47812d2dbcd56765c6651b04e0
FlagerLee <46860743+FlagerLee@users.noreply.github.com>
2019-05-03 15:46:47 +0800

    Add files via upload

---
+++ CG_debug.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+void throw_error ( int v , std::string s = "" )
+{
+    switch ( v )
+    {
+        case 1 :
+        {
+            std::cout << "Error: Division by zero\n" ;
+            break ;
+        }
+        case 2 :
+        {
+            std::cout << "Error: Placeholder missing\n" ;
+            break ;
+        }
+        case 3 :
+        {
+            std::cout << "Error: No matching operator for '" << s << "'\n" ;
+            break ;
+        }
+        case 4 :
+        {
+            std::cout << "Error: LOG operator's input must be positive\n" ;
+            break ;
+        }
+        case 5 :
+        {
+            std::cout << "Error: Cannot evaluate in an expression\n" ;
+            break ;
+        }
+        case 6 :
+        {
+            std::cout << "Error: Cannot evaluate a non-Placeholder or a non-Var type node\n" ;
+            break ;
+        }
+        case 7 :
+        {
+            std::cout << "Error: Cannot define a variate by itself\n"
+            break ;
+        }
+        case 8 :
+        {
+            std::cout << "Error: Cannot compute. Please check if you have added new operators. "
+        }
+        default :
+        {
+            std::cout << "Unknown type error! Please check if you give function 'throw_error' correct parameters\n" ;
+            break ;
+        }
+    }
+}
---
+++ CG_debug.h
@@ -0,0 +1,20 @@
+#ifndef __COMPUTATIONALGRAPH_DEBUG_H__
+#define __COMPUTATIONALGRAPH_DEBUG_H__
+
+#include <string>
+
+void throw_error ( int v , std::string s ) ;
+
+/* throw_error : 抛出错误
+ * 1. division by 0
+ * 2. placeholder missing
+ * 3. No matching operators for s 
+ * 4. LOG's input must be positive
+ * 5. Cannot evaluate in an expression
+ * 6. Cannot evaluate a non-Placeholder or a non-Var type node
+ * 7. Cannot evaluate a var by itself
+ * 8. Cannot compute
+ * 9. leaf node must be placeholder/constant/var_constant
+*/
+
+#endif
